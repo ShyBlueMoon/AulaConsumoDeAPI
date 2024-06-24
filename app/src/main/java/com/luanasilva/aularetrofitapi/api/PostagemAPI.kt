1a5e5d4448f6a8f477885fd5f153fa5127569d38
Luana Silva <luana.silva.11@hotmail.com>
2024-06-24 11:47:56 -0300

    API com Query

--- app/src/main/java/com/luanasilva/aularetrofitapi/api/PostagemAPI.kt
+++ app/src/main/java/com/luanasilva/aularetrofitapi/api/PostagemAPI.kt
@@ -3,15 +3,23 @@ package com.luanasilva.aularetrofitapi.api
 import com.luanasilva.aularetrofitapi.model.Comentario
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface PostagemAPI {
 
 
     //Recuperando postagem de id 1
 
-    @GET("posts/{id}/comments")
+    /*@GET("posts/{id}/comments")
     suspend fun recuperarComentariosParaPostagem(
         @Path ("id") id:Int
     ): Response<List<Comentario>>
+*/
+    @GET("comments") //comments?postId=1
+    suspend fun recuperarComentariosParaPostagemQuery(
+        @Query ("postId") id:Int
+    ): Response<List<Comentario>>
+
+
+
 }
